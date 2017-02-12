84cf42cfd1e6458b42c5ff6ee2ecd679a6ddb382
BenB0B0 <bob03295@sjfc.edu>
2017-02-12 02:17:52 -0500

    First commit

---
+++ Assets/Maps/base tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="red_collision" tilewidth="14" tileheight="14" spacing="1" tilecount="4" columns="2">
+ <image source="../../../../red.png" width="32" height="32"/>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="13.75" height="14"/>
+  </objectgroup>
+ </tile>
+</tileset>
